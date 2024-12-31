3b2a3e148b343c26d1b2fd745f722c6d471718d8
Shak <73081541+Shakoras@users.noreply.github.com>
2024-12-31 05:07:40 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.frontend_aks_cluster_name
+  name                = var.aks_cluster_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "frontend-k8s"
